07ecdbe06d6d65c66baec6c0a3aa8c36c5cb2165
B Shyam Sunder reddy <b.shyamsunder2004@gmail.com>
2025-02-11 20:19:40 +0530

    Create Height of Tree.java

---
+++ Height of Tree.java
@@ -0,0 +1,72 @@
+Given an array of unique elements, construct a Binary Search Tree and find the height of the tree.
+
+Input Format
+The first line of input contains T - the number of test cases. It is followed by 2T lines. The first line of each test case contains N - the number of nodes in the BST. The next line contains N unique integers - value of the nodes.
+
+Output Format
+For each test case, print the height of the Binary Search Tree, separated by a newline.
+
+Constraints
+1 <= T <= 1000
+1 <= N <= 1000
+0 <= ar[i] <= 10000
+
+Example
+Input
+3
+5
+1 2 3 4 5
+5
+3 2 4 1 5
+7
+4 5 15 0 1 7 17
+
+Output
+4
+2
+3
+
+import java.io.*;
+import java.util.*;
+class Node{
+    int data;
+    Node left,right;
+    Node(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
+}
+class binarysearchtree{
+    Node root;
+    public binarysearchtree(){
+        this.root=null;
+    }
+    
+    public Node insert(Node root,int b){
+        if(root==null) return new Node(b);
+        if(root.data<b) root.right=insert(root.right,b);
+        else if(root.data>b) root.left=insert(root.left,b);
+        return root;
+    }
+    public int height(Node root){
+        if(root==null) return -1;
+        return Math.max(height(root.left),height(root.right))+1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        while(t-->0){
+            int n=s.nextInt();
+            int a[]=new int[n];
+            binarysearchtree bst=new binarysearchtree();
+            for(int i=0;i<n;i++){
+                bst.root=bst.insert(bst.root,s.nextInt());
+            }
+            System.out.println(bst.height(bst.root));
+        }
+    }
+}
